e8bcf0ecf5ad761acd145cf317f1e2efca67a3d7
parthshinde850-crypto <parthshinde850@gmail.com>
2026-07-28 20:10:07 +0530

    Add files via upload

---
+++ 01-Basics/NewLine.java
@@ -0,0 +1,10 @@
+public class NewLine {
+    public static void main (String args[]){
+        System.out.println("Hello World"); //new line by println
+        System.out.print("Parth is here\nwhat the task"); // new line by \n
+        System.out.print("Learning java\n"); 
+        System.out.print("Hard to understand");
+        
+    }
+    
+}
