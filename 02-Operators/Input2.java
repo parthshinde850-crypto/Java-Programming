3bbaec6f4c8ee56f459173c064ca3971e706d5f4
parthshinde850-crypto <parthshinde850@gmail.com>
2026-07-28 20:15:00 +0530

    Add files via upload

---
+++ 02-Operators/Input2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Input2 {
+    //Input
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();  // Take whole line
+        //nextfloat
+        //nextint
+        
+        System.out.println(name);
+   
+        sc.close();  // not use then 5 line sc will show yellow underline but not error warning for not close sc
+
+    }
+    
+}
