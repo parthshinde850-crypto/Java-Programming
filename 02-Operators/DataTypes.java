4a8aed1b281a844c6d4a88cd644a8dc3d855c5b4
parthshinde850-crypto <parthshinde850@gmail.com>
2026-07-28 20:14:19 +0530

    Add files via upload

---
+++ 02-Operators/DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes{
+    // variable and int data type for different operators
+    public static void main(String[] args){
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        System.out.println(sum);
+
+        a = 10 ; // can change value for same varible
+        b= 5;
+        sum = a + b;
+        System.out.println(sum);
+
+        a = 29;
+        b= 10;
+        int diff = b-a;
+        System.out.println(diff);
+
+        a = 10;
+        b = 10;
+        int mul = a*b;
+        System.out.println(mul);
+
+    }
+}
