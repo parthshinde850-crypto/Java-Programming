0617d16d2cb81865f97864c8e04c6840debfcc06
parthshinde850-crypto <parthshinde850@gmail.com>
2026-07-30 21:17:40 +0530

    Add files via upload

---
+++ 04-Loops/Even_no.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class even_no {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no till want even no:");
+        int n = sc.nextInt();
+       
+
+
+        for (int i = 0; i <= n; i = i +2) {
+            System.out.println(i);
+            sc.close();
+        
+
+            }
+        }
+        
+    }
+    
+
