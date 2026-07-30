04a901c7773afc7059437ac51a9942f2a9fd1825
parthshinde850-crypto <parthshinde850@gmail.com>
2026-07-30 21:18:55 +0530

    Add files via upload

---
+++ 04-Loops/For_loop.java
@@ -0,0 +1,9 @@
+public class for_loop {
+    public static void main(String[] args){
+
+        for(int counter = 0; counter < 4; counter = counter + 1){
+            System.out.println("Hello World");
+        }
+    }
+    
+}
