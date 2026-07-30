da0cffabcac2ae3b3596a528ebd8062a13d4b4ce
parthshinde850-crypto <parthshinde850@gmail.com>
2026-07-30 21:18:34 +0530

    Add files via upload

---
+++ 04-Loops/First_to_10_print.java
@@ -0,0 +1,14 @@
+public class first_to_10_print {
+    public static void main(String[] args){
+
+        // counter = counter + 1 == counter ++
+        for (int counter = 0; counter < 11; counter ++){
+            // System.out.println(counter);
+            System.out.print(counter+" ");   // print in single line
+
+
+  
+        }
+    }
+    
+}
