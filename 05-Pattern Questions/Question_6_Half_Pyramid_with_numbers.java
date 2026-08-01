ec635d1b4a0ca5a86dde3ce643a151c4ee43c1bf
parthshinde850-crypto <parthshinde850@gmail.com>
2026-08-01 16:31:03 +0530

    Add files via upload

---
+++ 05-Pattern Questions/Question_6_Half_Pyramid_with_numbers.java
@@ -0,0 +1,22 @@
+// Question 6 = print the pattern
+//  1 
+//  1 2
+//  1 2 3
+//  1 2 3 4
+//  1 2 3 4 5 
+
+
+public class Question_6_Half_Pyramid_with_numbers {
+
+    public static void main(String[] args) {
+        
+        int n = 5;
+
+        for (int i = 1; i <= n; i = i + 1) {
+            for (int j = 1; j <= i ; j = j +1) {
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
