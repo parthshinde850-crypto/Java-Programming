b80d2b15b6ac10504b4c1bf5a979993db039f1ff
parthshinde850-crypto <parthshinde850@gmail.com>
2026-08-01 16:31:43 +0530

    Add files via upload

---
+++ 05-Pattern Questions/Question_8_Floyds_Triange.java
@@ -0,0 +1,25 @@
+// Question 8 = print the pattern
+//  1 
+//  2 3 
+//  4 5 6
+//  7 8 9 10
+// 11 12 13 14 15
+
+
+public class Question_8_Floyds_Triange {
+    public static void main(String[] args) {
+
+        int n = 5;
+        int number = 1;
+
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number+ " ");
+                number++; // number = number + 1
+
+            }
+            System.out.println();
+        }
+    }
+    
+}
